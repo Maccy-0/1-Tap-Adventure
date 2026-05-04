53c3cadf9088acb4924355e8dd041e00b6f548c5
Remaek <brooksmt@sheridancollege.ca>
2026-05-04 19:16:16 -0400

    MainMenu_Button_Function

--- Scenes/main_menu_scene.gd
+++ Scenes/main_menu_scene.gd
@@ -1,22 +1,43 @@
 extends Control
 
+var heldDown = false
+var heldTime = 0.0
+var confDuration = 1.0
+var pressTime = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/Tutorial.grab_focus.call_deferred()
 
 func _input(event):
+	if event is InputEventKey and event.is_pressed() and heldDown == false:
+		if event.keycode == KEY_SPACE:
+			heldDown = true
+			pressTime = Time.get_ticks_msec() / 1000.0
+
 	if event is InputEventKey and event.is_released():
-		if event.keycode == KEY_F:
+		if event.keycode == KEY_SPACE:
+			heldTime = 0.0
+			pressTime = 0.0
+			heldDown = false
 			var a = InputEventAction.new()
 			a.action = "ui_focus_next"
 			a.pressed = true
 			Input.parse_input_event(a)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
+func _process(delta: float) -> void:
+	if heldDown == true:
+		heldTime = Time.get_ticks_msec() / 1000.0 
+		var compare = heldTime - pressTime
+		print_debug(compare)
+		if compare >= confDuration:
+			print_debug("Held Time Finished")
+			heldDown = false
+			var a = InputEventAction.new()
+			a.action = "ui_accept"
+			a.pressed = true
+			Input.parse_input_event(a)
 
 func _on_tutorial_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/MainScene.tscn")
